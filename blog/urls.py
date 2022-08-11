289989444767c1aa568077c0b2ba4363a1c17205
leehg9805 <dlgorkd1234@naver.com>
2022-08-11 09:38:21 +0900

    CBV로 블로그 포스트 목록 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 
 urlpatterns = [
-    path('<int:pk>/', views.single_post_page),
-    path('', views.index),
+    path('', views.PostList.as_view()),
+    #path('<int:pk>/', views.single_post_page),
+
+
 
 ]
