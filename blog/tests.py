6c4f590f349465fa44f1f29bf025a552bd1be116
leehg9805 <dlgorkd1234@naver.com>
2022-08-12 16:51:29 +0900

    포스트 목록 페이지와 포스트 상세 페이지 디자인 통일

--- blog/tests.py
+++ blog/tests.py
@@ -23,9 +23,9 @@ class TestView(TestCase):
         self.assertEqual(Post.objects.count(), 0)
 
         main_area = soup.find('div', id='main-area')
-        self.assertIn('아직 게시물 없습니다.', main_area.text)
+        self.assertIn('아직 게시물이 없습니다.', main_area.text)
 
-        post_001 = Post.object.create(
+        post_001 = Post.objects.create(
             title='첫 번째 포스트입니다.',
             content='Hello World. We are the world',
         )
@@ -33,7 +33,7 @@ class TestView(TestCase):
             title='두 번째 포스트입니다.',
             content='1등이 전부는 아니잖아요?',
         )
-        self.assertSetEqual(Post.object.count(), 2)
+        self.assertEqual(Post.objects.count(), 2)
 
         response = self.client.get('/blog/')
         soup = BeautifulSoup(response.content, 'html.parser')
@@ -44,5 +44,31 @@ class TestView(TestCase):
         self.assertIn(post_002.title, main_area.text)
 
         self.assertNotIn('아직 게시물이 없습니다.', main_area.text)
+
+        def test_post_detail(self):
+            post_001 = Post.objects.create(
+                title= '첫 번째 포스트입니다.',
+                content= 'Hello World. We are the world',
+            )
+
+            self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
+
+            response = self.client.get(post_001.get_absolute_url())
+            self.assertEqual(response.status_code, 200)
+            soup = BeautifulSoup(response.content, 'html.parser')
+
+            navbar = soup.nav
+            self.assertIn('Blog', navbar.text)
+            self.assertIn('About Me', navbar.text)
+
+            self.assertIn(post_001.title, soup.title.text)
+
+            main_area = soup.find('div', id='main-area')
+            post_area = main_area.find('div', id='post_area')
+            self.assertIn(post_001.title, post_area.text)
+
+            self.assertIn(post_001.content, post_area.text)
+
+
 
 # Create your tests here.
