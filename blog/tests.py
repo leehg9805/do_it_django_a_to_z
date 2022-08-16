9a0eb03848cfdd723e6455d58bde2f7020f127ce
leehg9805 <dlgorkd1234@naver.com>
2022-08-16 09:41:15 +0900

    내비 푸터 모듈화

--- blog/tests.py
+++ blog/tests.py
@@ -6,6 +6,23 @@ class TestView(TestCase):
     def setUp(self):
         self.client = Client()
 
+    def navbar_test(self, soup):
+        navbar = soup.nav
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About Me', navbar.text)
+
+        logo_btn = navbar.find('a', text='스마트 부산')
+        self.assertEqual(logo_btn.attrs['href'], '/')
+
+        home_btn = navbar.find('a', text='Home')
+        self.assertEqual(home_btn.attrs['href'], '/')
+
+        blog_btn = navbar.find('a', text='Blog')
+        self.assertEqual(blog_btn.attrs['href'], '/blog/')
+
+        about_me_btn = navbar.find('a', text='About Me')
+        self.assertEqual(about_me_btn.attrs['href'], '/About Me/')
+
     def test_post_list(self):
 
         response = self.client.get('/blog/')
@@ -14,11 +31,7 @@ class TestView(TestCase):
 
         soup = BeautifulSoup(response.content, 'html.parser')
         self.assertEqual(soup.title.text, 'Blog')
-
-        navbar = soup.nav
-
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About Me', navbar.text)
+        self.navbar_test(soup)
 
         self.assertEqual(Post.objects.count(), 0)
 
@@ -45,29 +58,26 @@ class TestView(TestCase):
 
         self.assertNotIn('아직 게시물이 없습니다.', main_area.text)
 
-        def test_post_detail(self):
-            post_001 = Post.objects.create(
+    def test_post_detail(self):
+        post_001 = Post.objects.create(
                 title= '첫 번째 포스트입니다.',
                 content= 'Hello World. We are the world',
             )
 
-            self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
+        self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
 
-            response = self.client.get(post_001.get_absolute_url())
-            self.assertEqual(response.status_code, 200)
-            soup = BeautifulSoup(response.content, 'html.parser')
+        response = self.client.get(post_001.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
 
-            navbar = soup.nav
-            self.assertIn('Blog', navbar.text)
-            self.assertIn('About Me', navbar.text)
+        self.assertIn(post_001.title, soup.title.text)
+        self.navbar_test(soup)
 
-            self.assertIn(post_001.title, soup.title.text)
+        main_area = soup.find('div', id='main-area')
+        post_area = main_area.find('div', id='post_area')
+        self.assertIn(post_001.title, post_area.text)
 
-            main_area = soup.find('div', id='main-area')
-            post_area = main_area.find('div', id='post_area')
-            self.assertIn(post_001.title, post_area.text)
-
-            self.assertIn(post_001.content, post_area.text)
+        self.assertIn(post_001.content, post_area.text)
 
 
 
