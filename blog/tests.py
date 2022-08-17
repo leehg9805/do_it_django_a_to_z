67d54014bf6c1e6747b9b2834887703c75f845ff
leehg9805 <dlgorkd1234@naver.com>
2022-08-17 15:40:18 +0900

    스태프 권한 계정으로 확인하기

--- blog/tests.py
+++ blog/tests.py
@@ -10,6 +10,8 @@ class TestView(TestCase):
                                                    password='1q2w3e4r!')
         self.user_obama = User.objects.create_user(username='obama',
                                                    password='1q2w3e4r!')
+        self.user_obama.is_staff = True
+        self.user_obama.save()
 
         self.category_programming = Category.objects.create(name='programming',
                                                             slug='programming')
@@ -79,7 +81,10 @@ class TestView(TestCase):
         self.assertNotEqual(response.status_code, 200)
 
         self.client.login(username='trump', password='1q2w3e4r!')
+        response = self.client.get('/blog/create_post/')
+        self.assertNotEqual(response.status_code, 200)
 
+        self.client.login(username='obama', password='1q2w3e4r!')
         response = self.client.get('/blog/create_post/')
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
@@ -98,7 +103,7 @@ class TestView(TestCase):
         self.assertEqual(Post.objects.count(), 4)
         last_post = Post.objects.last()
         self.assertEqual(last_post.title, "Post Form 만들기")
-        self.assertEqual(last_post.author.username, 'trump')
+        self.assertEqual(last_post.author.username, 'obama')
 
 
     def navbar_test(self, soup):
