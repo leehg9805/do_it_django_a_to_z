d1b268baefe317d6cee1c5ae50f16b1ad0350b63
leehg9805 <dlgorkd1234@naver.com>
2022-08-17 10:10:29 +0900

    카테고리 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 
 urlpatterns = [
+    path('category/<str:slug>/', views.category_page),
     path('', views.PostList.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()),
 ]
